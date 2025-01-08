e3e2eabe8a768a128ef4030c588d1793755b2767
Staylth <raghavsmashyal@gmail.com>
2025-01-08 19:13:21 +0530

    Basic DOM Manipulation done

--- Basics/JS/JS in browser/script.js
+++ Basics/JS/JS in browser/script.js
@@ -15,3 +15,31 @@ document.getElementById("HighlightFirstCity").
     let firstElement = document.getElementById("CityList");
     firstElement.firstElementChild.classList.add("highlight");
   })
+
+//example 3
+//Change the DOM element to Espresso 
+document.getElementById("coffeeButton").
+  addEventListener('click', function () {
+    let el = document.getElementById('coffeeType');
+    el.textContent = 'Espresso'
+  })
+
+
+
+// example 4
+//create and insert an element 
+document.getElementById('createNewElementButton').
+  addEventListener('click', function () {
+    let newElement = document.createElement('li');
+    newElement.textContent = "KissanJam";
+    document.getElementById('bakeryList').
+      appendChild(newElement);
+  })
+
+// example-5
+//Removing elements
+document.querySelector('.bikeButton').
+  addEventListener('click', function () {
+    let bikePartList = document.getElementById('bikePartList');
+    bikePartList.lastElementChild.remove();
+  })
